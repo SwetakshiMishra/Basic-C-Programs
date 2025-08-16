8d7af7542b9c40a0d2d27e63ce3f07cc0149cb56
SwetakshiMishra <swetakshimishra@gmail.com>
2025-08-16 04:56:37 +0000

    Add Sum of Digits Program

---
+++ Sumofdigits.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+// Sum of digits of a number
+class Number {
+    int num;
+
+public:
+    void number_entry() {
+        cout << "Enter the number you want to sum the digits of: ";
+        cin >> num;
+    }
+
+    void digits_accessing() {
+        int sum = 0;
+        int temp = num;
+
+        while (temp > 0) {
+            sum += temp % 10;
+            temp /= 10;
+        }
+
+        cout << "The sum of digits of " << num << " is " << sum << endl;
+    }
+};
+
+int main() {
+    Number obj;
+    obj.number_entry();
+    obj.digits_accessing();
+    return 0;
+}
